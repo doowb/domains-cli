71cbb9c386215211bb974d7113703b171e6b50d0
doowb <brian.woodward@gmail.com>
2017-01-12 19:29:32 -0500

    move code and use dnsimple client

--- cli.js
+++ cli.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 
+var utils = require('./lib/utils');
 var checkDomains = require('./');
-var writeJson = require('write-json');
-var log = require('log-utils');
-var Store = require('data-store');
-var store = new Store('domains-cli');
+
+var store = new utils.Store('domains-cli');
 var auth = store.get('auth');
 
-var argv = require('yargs-parser')(process.argv.slice(2), {
+var argv = utils.yargs(process.argv.slice(2), {
   alias: {
     dest: 'd',
     fast: 'f',
@@ -25,10 +24,10 @@ if (auth.token) {
   store.set('auth', auth);
 } else {
   console.error();
-  console.error(log.bold('  please specify authentication details'));
-  console.error(log.gray('  authentication tokens can be created for an account at https://dnsimple.com'));
+  console.error(utils.log.bold('  please specify authentication details'));
+  console.error(utils.log.gray('  authentication tokens can be created for an account at https://dnsimple.com'));
   console.error();
-  console.error(log.bold('  --token, -t'));
+  console.error(utils.log.bold('  --token, -t'));
   console.error();
   process.exit(1);
 }
@@ -37,7 +36,7 @@ if (argv._.length === 0) {
   console.error();
   console.error('  Enter the domains to search');
   console.error();
-  console.error('    $', log.colors.cyan('domains <list of domains>'));
+  console.error('    $', utils.log.colors.cyan('domains <list of domains>'));
   console.error();
   process.exit();
 }
@@ -47,7 +46,7 @@ argv.auth = auth;
 checkDomains(argv._, argv, function(err, res) {
   if (err) return handleError(err);
   if (argv.dest) {
-    writeJson(argv.dest, res, function(err) {
+    utils.write(argv.dest, res, function(err) {
       if (err) return handleError(err);
       end();
     });
--- index.js
+++ index.js
@@ -1,188 +1,35 @@
 'use strict';
 
-var pad = require('pad-right');
-var reduce = require('async-array-reduce');
-var extend = require('extend-shallow');
-var request = require('request');
-var unique = require('array-unique');
-var longest = require('longest');
-var utils = require('log-utils');
+var utils = require('./lib/utils');
 
-var baseUrl = 'https://api.dnsimple.com/v2';
-
-module.exports = checkDomains;
-
-function checkDomains(domains, options, cb) {
+module.exports = function(domains, options, cb) {
   if (typeof options === 'function') {
     cb = options;
     options = {};
   }
-  var opts = extend({baseUrl: baseUrl}, options);
-  var authOpts = authOptions(opts);
+  var opts = utils.extend({
+    baseUrl: 'https://api.dnsimple.com/v2',
+    tlds: ['com', 'io']
+  }, options);
 
-  whoami(opts.baseUrl, authOpts, function(err, id) {
-    if (err) return cb(err);
-    authOpts.id = id;
-    authOpts.tlds = arrayify(opts.tlds).reduce(function(acc, tld) {
+  var tlds = utils.arrayify(opts.tlds)
+    .reduce(function(acc, tld) {
       return acc.concat(tld.split(','));
     }, []);
 
-    if (!authOpts.tlds.length) {
-      authOpts.tlds = ['com', 'io'];
-    }
-
-    var arr = opts.fast ? domains : createDomains(domains);
-    var len = longest(arr).length;
-
-    reduce(arr, {}, function(acc, domain, next) {
-      checkDomain(opts.baseUrl, domain, authOpts, function(err, available) {
-        if (err) return next(err);
-
-        if (opts.silent !== true) {
-          logStatus(domain, available, len);
-        }
-        acc[domain] = available;
-        next(null, acc);
-      });
-    }, cb);
+  var dnsimple = utils.dnsimple({
+    baseUrl: opts.baseUrl,
+    accessToken: opts.auth.token
   });
-}
 
-function checkDomain(baseUrl, domain, options, cb) {
-  var opts = {
-    headers: options.headers
-  };
+  domains = opts.fast ? domains : utils.createDomains(domains);
+  opts.len = utils.longest(domains).length;
 
-  reduce(options.tlds, {}, function(acc, tld, next) {
-    // /:account/registrar/domains/:domain/check
-    var url = `${baseUrl}/${options.id}/registrar/domains/${domain}.${tld}/check`;
-    try {
-      request(url, opts, function(err, res, body) {
-        if (err) return next(err);
-        if (res && res.statusCode > 400) {
-          return next(new Error(res.statusMessage));
-        }
-
-        try {
-          var data = JSON.parse(body);
-          if (data.message) {
-            return next(new Error(data.message));
-          }
-          acc[tld] = data.data.available;
-          next(null, acc);
-        } catch (err) {
-          next(err);
-        }
-      });
-    } catch (err) {
-      next(err);
-    }
-  }, cb);
-}
-
-function whoami(baseUrl, options, cb) {
-  request(`${baseUrl}/whoami`, options, function(err, res, body) {
-    if (err) return cb(err);
-    if (res.statusCode > 400) {
-      return cb(new Error(res.statusMessage));
-    }
-    try {
-      var data = JSON.parse(body).data;
-      cb(null, data.account.id);
-    } catch(err) {
-      cb(err);
-    }
-  });
-}
-
-function logStatus(str, available, len) {
-  var gray = utils.colors.gray;
-  var domain = pad(str, len, ' ');
-  var isAvailable = false;
-  var cols = Object.keys(available).map(function(tld) {
-    if (available[tld]) isAvailable = true;
-    return gray(' | ') + color(tld, available[tld]);
-  });
-  var rows = [success(domain, isAvailable)].concat(cols);
-  console.log(rows.join(''));
-}
-
-function color(registry, val) {
-  if (val === true) {
-    return utils.colors.green(registry) + ' ' + utils.success;
-  } else {
-    return registry + ' ' + utils.error;
-  }
-}
-
-function success(domain, val) {
-  if (val === true) {
-    domain = utils.colors.green(domain);
-  }
-  return domain;
-}
-
-function createDomains(domains) {
-  var arr = arrayify(domains)
-    .reduce(function(acc, str) {
-      acc = acc.concat(combinations(str));
-      acc = acc.concat(str.split(/\W/));
-      var domain = uncamelcase(str);
-      if (domain !== str) {
-        acc.push(domain);
-      }
-      return acc.concat(str);
-    }, [])
-    .filter(function(str) {
-      return !/\s/.test(str);
+  dnsimple.identity.whoami()
+    .then(function(response) {
+      var account = response.data.account;
+      var checkDomain = utils.checkDomain(dnsimple, account, tlds, opts);
+      utils.reduce(domains, {}, checkDomain, cb);
     })
-    .map(function(str) {
-      return encodeURIComponent(normalize(str));
-    });
-
-  arr.sort();
-  return unique(arr.filter(Boolean));
-}
-
-function combinations(str) {
-  var arr = str.split(/\W+/);
-  var len = arr.length;
-  var res = [];
-  var idx = -1;
-
-  while (++idx < len) {
-    for (var j = 0; j <= idx; j++) {
-      res.push(arr.slice(j, len - idx + j).join('-'));
-    }
-  }
-  return res;
-}
-
-/*
-curl  -H 'Authorization: Bearer <token>' \
-      -H 'Accept: application/json' \
-      https://api.dnsimple.com/v2/whoami
- */
-function authOptions(options) {
-  return {
-    headers: {
-      'Authorization': `Bearer ${options.auth.token}`,
-      'Accept': 'application/json'
-    }
-  };
-}
-
-function normalize(domain) {
-  return domain.toLowerCase().replace(/^\W+|\W+$/g, '');
-}
-
-function uncamelcase(domain) {
-  if (domain.length === 1) return domain.toLowerCase();
-  return domain.replace(/(?!(^|\W))[A-Z]/, function(ch) {
-    return '-' + ch.toLowerCase();
-  });
-}
-
-function arrayify(val) {
-  return val ? (Array.isArray(val) ? val : [val]) : [];
+    .catch(cb);
 }
